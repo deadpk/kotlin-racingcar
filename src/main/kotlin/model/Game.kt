7fdb338801fd557449159950ac4f0299f5f190ea
teo.stra <teo.stra@kakaocorp.com>
2022-05-16 21:16:54 +0900

    step5: 리뷰 반영
    입력값 검증 추가

--- src/main/kotlin/model/Game.kt
+++ src/main/kotlin/model/Game.kt
@@ -23,11 +23,33 @@ class Game {
 
     private fun askNamesOfCar(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        return readLine()!!.split(",").map { it.trim() }
+        val names = readLine()!!.split(",")
+            .map { name ->
+                name.trim().also {
+                    validateName(it)
+                }
+            }
+        return names
+    }
+
+    private fun validateName(name: String) {
+        when {
+            name.isNullOrBlank() -> throw IllegalArgumentException("Blank name not allowed")
+            name.length > 5 -> throw IllegalArgumentException("Name too long")
+        }
     }
 
     private fun askNumOfRounds(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return readLine()!!.toInt()
+        val num = readLine()!!.toInt().also {
+            validateNum(it)
+        }
+        return num
+    }
+
+    private fun validateNum(num: Int) {
+        when {
+            num < 1 -> throw IllegalArgumentException("Should larger than 0")
+        }
     }
 }
